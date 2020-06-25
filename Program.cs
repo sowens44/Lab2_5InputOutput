2d9a3178605d6245668b81193c8c9b2ec8cb968b
so2owens@gmail.com <so2owens@gmail.com>
2020-06-25 16:58:25 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab2_5InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lab2_5 New Changes today 
+            Console.WriteLine("Hello World!");
+            // 1
+            int intFirstNumber = 0;
+            int intSecondNumber = 0;
+            int intResult = 0;
+            double dblResult = 0;
+
+
+            Console.WriteLine("Enter your First Number: ");
+            intFirstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your Second Number: ");
+            intSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Adding
+            intResult = intFirstNumber + intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} plus {intSecondNumber} is {intResult}");
+
+            //Subtracting
+            intResult = intFirstNumber - intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} minus {intSecondNumber} is {intResult}");
+
+            //Multiplying
+            intResult = intFirstNumber * intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} times {intSecondNumber} is {intResult}");
+
+            //Dividing
+            intResult = intFirstNumber / intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} Divided by {intSecondNumber} is {intResult}");
+
+        }
+    }
+}
